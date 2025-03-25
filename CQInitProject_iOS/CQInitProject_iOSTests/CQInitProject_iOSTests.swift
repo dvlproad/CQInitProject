a1786c6517cd7e4da04e5e55aea14fabebb3902c
dvlproad <dvlproad@163.com>
2025-03-25 20:53:34 +0800

    【Feature】创建 CQInitProject_iOS

---
+++ CQInitProject_iOS/CQInitProject_iOSTests/CQInitProject_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  CQInitProject_iOSTests.swift
+//  CQInitProject_iOSTests
+//
+//  Created by qian on 2025/3/25.
+//
+
+import Testing
+@testable import CQInitProject_iOS
+
+struct CQInitProject_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
